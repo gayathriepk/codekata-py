d3244a9d430fc79b19f4bb4ec21a809530dfeacc
gayathriepk <gayathriepk18@gmail.com>
2017-08-05 18:57:24 +0530

    c program

---
+++ GITHUBC.C
@@ -0,0 +1,21 @@
+void main()
+{
+ float n;
+ printf("\n Enter the number \n");
+ scanf("%f",&n);
+ if(n!=0)
+ {
+  if(n<0)
+  {
+   printf("Its negative");
+  }
+  else
+  {
+   printf("Its postive");
+  }
+ }
+ else
+ {
+  printf("Its zero");
+ }
+ }
